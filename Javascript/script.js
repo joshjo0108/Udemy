85bd091250a7d1f90bd6598ef850b542481069e5
Josh Jo <joshjo0108@gmail.com>
2021-12-14 10:45:33 +0800

    DATA TYPES

--- Javascript/script.js
+++ Javascript/script.js
@@ -15,5 +15,23 @@ if(teacherName == "Kalob") {
     console.log("I have",apples,"apples");
     console.log("My height is",height,".");
 }
+/*
+    THIS IS JUST GOING TO COMMENT 
+    ALL THE LINES!!
+*/
 
+// JAVASCRIPT DATA TYPES
+const str = "A string"; //STRING
+const num = 3;  // NUMBER
+const flt = 3.14; // FLOAT
+const bool = true; // OR 'false' <- BOOLEAN
+const list = ["ITEM 1",2,"THREE", 4.5, []]; //  ARRAY = LIST
+const thing = null; // HAS NOTHING IN IT
+const notDefined = undefined; //CURRENTLY HOLDS NO VALUE
+const getAge = function() {return 31;} // FUNCTION
+const obj = {   //LIKE A DICTIONARY
+    'KEY':'VALUE',
+    'name':'jae won jo',
+    'age': 30
+}   // CAN BE ACCESSED WITH -> obj['name']
 
