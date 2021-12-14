f9d509f1dd2ea331d6b4fcd76168e28725b12025
Josh Jo <joshjo0108@gmail.com>
2021-12-14 10:05:14 +0800

    START OF JAVASCRIPT

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+// OLD WAY OF WRITING JAVASCRIPT
+var website = "https://codingforeverybody.com/";
+// command + d: SELECTING THE SAME WORD
+var teacherName = "Kalob";
+console.log(teacherName);
+console.log(website);
+
+if(teacherName == "Kalob") {
+// CAN BE REASSIGNED
+    let apples = 1;
+// CANNOT BE REASSIGNED
+// \" TO LET KNOW ""
+    const height = "6'0\"";
+// HAS TO BE WITHIN THE CURLY BRACES
+    console.log("I have",apples,"apples");
+    console.log("My height is",height,".");
+}
+
+
