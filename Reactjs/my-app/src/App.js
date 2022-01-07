d71862da3acd9eb38467b4bda1616a7116ffe36a
Josh Jo <joshjo0108@gmail.com>
2022-01-07 16:20:01 +0800

    ON CLICK UPDATE DATA FROM API

--- Reactjs/my-app/src/App.js
+++ Reactjs/my-app/src/App.js
@@ -1,25 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+// import Item from './MyItem';
 
-class Item extends React.Component {
+
+class FilmItemRow extends React.Component {
   render() {
-    return (
-      <h1>THIS IS A WAY TO USE "class Item"</h1>
-      // <small>THIS IS JUST A TEST!</small>
+    return(
+      <li>
+        {/* PASS DATA FROM ONE COMPONENT TO OTHER COMPONENTS */}
+        <a href={this.props.url}>{this.props.url}</a>
+      </li>
     )
   }
 }
+
+class StarWars extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+    // SET AS DEFAULT
+      name: null,
+      height: null,
+      homeworld: null,
+      films: [],
+      loadedCharacter: false
+    }
+  }
+
+  getNewCharacter() {
+    console.log("get new character")
+    const randomNumber = Math.round( Math.random() * 82)
+    const url = `https://swapi.dev/api/people/${randomNumber}/`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          films: data.films,
+        // THIS GETS TRUE WHEN YOU CLICK ON THE BUTTON
+          loadedCharacter: true
+        })
+      })
+  }
+
+
+  render() {
+    const movies = this.state.films.map((film,i) => {
+      return <FilmItemRow key={i} url={film} />
+    })
+
+    return (
+      <div>
+        {
+          this.state.loadedCharacter &&
+            <div>
+              <h1>Name: {this.state.name}</h1>
+              <p>{this.state.height} cm</p>
+            {/* CURLY BRACES ARE ALWAYS JAVASCRIPT!!! */}
+            {/* LINKING */}
+              <p><a href={this.state.homeworld}>HomeWorld</a>: {this.state.homeworld}</p>
+            {/* WITHOUT <ul></ul> */}
+              {movies}
+            </div>
+        }
+        
+        <button 
+          type="button" 
+          onClick={() => this.getNewCharacter()} 
+          className='btn'>
+            Randomize Character
+        </button>
+      </div>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">             //STRAIGHT XHTML WITHIN JAVASCRIPT
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />  
-        <p>
+        {/* <p>
           LIVE CHANGE IN FRONT OF ME
-        </p>
-        <Item />
+        </p> */}
+        {/* <Item name="Aaron"/> */}
+        <StarWars />
         <a
           className="App-link"
           href="https://reactjs.org"
